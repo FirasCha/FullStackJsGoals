9d889f020126ad56edc4c4ee9255b8a74ab0b7e1
FirasCha <cri.firas@gmail.com>
2023-03-09 16:30:20 +0100

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../Models/userModel')
+
 //@desc REGISTER NEW USER
 //@route POST /api/users
 //@access Public
@@ -32,14 +33,14 @@ const registerUser = asyncHandler( async(req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
         throw new Error('Invalid user data')
     }
 })
-
 //@desc AUthenticate a USER
 //@route POST /api/users/login
 //@access Public
@@ -51,21 +52,26 @@ const loginUser = asyncHandler( async(req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
         throw new Error('Invalid credentials')
     }
 })
-
 //@desc GET USER DATA
 //@route GET /api/users/me
 //@access Public
 const getMe = asyncHandler( async(req, res) => {
     res.json({message : 'User data display'})
 })
-
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
 module.exports = {
     registerUser,
     loginUser,
