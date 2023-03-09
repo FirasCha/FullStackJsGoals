72731f7acd7c432ac31983f4195489845ed9659d
FirasCha <cri.firas@gmail.com>
2023-03-09 16:17:13 +0100

    login user controller without jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,7 +28,6 @@ const registerUser = asyncHandler( async(req, res) => {
         email,
         password: hashedPassword
     })
-
     if(user){
         res.status(201).json({
             _id: user.id,
@@ -45,7 +44,19 @@ const registerUser = asyncHandler( async(req, res) => {
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler( async(req, res) => {
-    res.json({message : 'login user'})
+    const {email, password} = req.body
+    //Check for user email
+    const user = await User.findOne({email})
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 //@desc GET USER DATA
